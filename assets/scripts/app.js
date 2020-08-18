23087e94e963061397d6def39cb1f453c78a5cb1
kelibst <kboster17@gmail.com>
2020-08-18 06:58:27 +0000

    added search input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,12 @@
 const dropdowntoggler = document.querySelector('.dropdown-toggler');
+const headerContainer= document.querySelector('.header-container');
+const hiddenInp = document.querySelector('.searcher-form');
 
-
+headerContainer.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('search-icon')){
+        hiddenInp.classList.toggle('d-none');
+    }
+})
 
 dropdowntoggler.addEventListener('click', function (){
     const position = document.querySelector('.drop-container');
